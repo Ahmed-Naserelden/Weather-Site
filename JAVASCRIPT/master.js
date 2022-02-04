bde3ee4b1c9cb8b05042c97adf1d5184773ce935
ahmed <ahmednsereldin@gmail.com>
2022-02-04 02:48:41 +0200

    Oop

--- JAVASCRIPT/master.js
+++ JAVASCRIPT/master.js
@@ -1,3 +1,4 @@
+//#region 
 // function calc(nums){
 //     var res =  0;
 //     for(var i = 0; i < nums.length; i++){
@@ -26,7 +27,6 @@
 // console.log("Allah Akbar");
 // arrow function{
 // Arrays
-
 // let arr = [4, 5, 9, 10, 3, 2];
 // arr.sort(function (a, b){return a - b;}).reverse();
 // for(let i = 0; i < arr.length; i++){
@@ -40,37 +40,68 @@
 //         console.log(arr[i]);
 //     }
 // }
-
-
 // Aplication Programing Interface
-
-
-let arr = [4, 5, 9, 10, 3, 2];
-let arr1 = [4, 5, 9, 10, 3, 2];
-arr = arr.join('');
-console.log(arr)
-arr
-function show(){
-    document.getElementById("demo").innerHTML = arr;
-    arr = arr.concat(arr);
-}
-mainloop:for(let i = 1; i <= 10; i++){
-    nested:for(let j = 1; j <= 10; j++){
-        console.log(i, j);
-        if(j == 5)
-            break nested;
-        else if(i == 5)
-            break mainloop;
-        
-    }
-}
-document.querySelector("#demo").innerHTML = "<h1>A</h1>";
-document.getElementById("cli").onclick = function (){
-    let x = document.querySelector("#demo").style.color;
-    if(x != "red"){
-        document.querySelector("#demo").style.color = "red";
-        document.querySelector("#demo").style.background = "green";
-    }
-    else
-        document.querySelector("#demo").style.color = "green";
-} 
+// let arr = [4, 5, 9, 10, 3, 2];
+// let arr1 = [4, 5, 9, 10, 3, 2];
+// arr = arr.join('');
+// console.log(arr)
+// arr
+// function show(){
+//     document.getElementById("demo").innerHTML = arr;
+//     arr = arr.concat(arr);
+// }
+// mainloop:for(let i = 1; i <= 10; i++){
+//     nested:for(let j = 1; j <= 10; j++){
+//         console.log(i, j);
+//         if(j == 5)
+//             break nested;
+//         else if(i == 5)
+//             break mainloop;    
+//     }
+// }
+// document.querySelector("#demo").innerHTML = "<h1>A</h1>";
+// document.getElementById("cli").onclick = function (){
+//     let x = document.querySelector("#demo").style.color;
+//     if(x != "red"){
+//         document.querySelector("#demo").style.color = "red";
+//         document.querySelector("#demo").style.background.color = "green";
+//     }
+//     else
+//         document.querySelector("#demo").style.color = "green";
+// } 
+// let user = {
+//     name : 'ahmed',
+//     age : 19,
+//     skills: ["Reading", "Football", "Basktball"],
+//     available: true,
+//     addresses: {
+//         ksa: "Ridadh",
+//         egypt: {
+//             one: "Cairo",
+//             two: "Giza",
+//         },
+//     },
+//     checkAv: function(){
+//         if(user.available === true){
+//             return `free for work`;
+//         }else{
+//             return `Not Free`;
+//         }
+//     },
+// };
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.available);
+// console.log(user.skills);
+// console.log(user.skills[0]);
+// console.log(user.skills[0]);
+// console.log(user.skills[1]);
+// console.log(user.addresses);
+// console.log(user.addresses.ksa);
+// console.log(user.addresses.egypt);
+// console.log(user.addresses.egypt.one);
+// console.log(user.addresses.egypt.two);
+// console.log(user.checkAv());
+// console.log(user.checkAv());
+// console.log(user.);
+//#endregion
