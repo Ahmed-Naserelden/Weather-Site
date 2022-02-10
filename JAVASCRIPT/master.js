4dba6c28447e5f6121c142044c9184a04484f527
ahmed <an2071497@gmail.com>
2022-02-11 00:37:50 +0200

    add new files api

--- JAVASCRIPT/master.js
+++ JAVASCRIPT/master.js
@@ -1,7 +1,3 @@
-const api ={
-    key: "17df00420ac53c2f7044906a5dd6664a",
-    base: "https://api.openweathermap.org/data/2.5/",
-}
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 btn.addEventListener("click", getInput);
@@ -22,33 +18,38 @@ function getData(){
 const error = document.querySelector(".error");
 error.textContent="";
 function displayData(response){
+    const city = document.querySelector(".city");
+    const today = new Date();
+    const date = document.querySelector(".date");
+    const temp = document.querySelector(".temp");
+    const weather = document.querySelector(".weather");
+    const tempRange = document.querySelector(".temp-range");
+    const weatherIcon = document.querySelector(".weather-icon");
     if(response.cod == "404"){
+        city.textContent="";
+        date.textContent="";
+        temp.textContent="";
+        weather.textContent="";
+        tempRange.textContent="";
+        weatherIcon.textContent="";
+        weatherIcon.src="";
         error.textContent = "Please Enter A Valid City";
         search.value="";
+
     }else{
         error.textContent="";
-        const city = document.querySelector(".city");
+        
         city.innerText = `${response.name}, ${response.sys.country}`;
-        const today = new Date();
-        const date = document.querySelector(".date");
+        
         date.innerText = dateFunction(today);
-
-        const temp = document.querySelector(".temp");
         temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°C</span>`;
-
-        const weather = document.querySelector(".weather");
         weather.innerText = `Weather: ${response.weather[0].main}`;
-
-        const tempRange = document.querySelector(".temp-range");
         tempRange.innerText = `Temp Range: ${Math.round(response.main.temp_min)}°C | ${Math.round(response.main.temp_max)}°C `;
-
-        const weatherIcon = document.querySelector(".weather-icon");
         const iconURL = "http://openweathermap.org/img/w/";
         weatherIcon.src = iconURL + response.weather[0].icon + ".png" ;
         search.value = "";
     }
 }
-
 function dateFunction (d){
     let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", 
     "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -60,3 +61,4 @@ function dateFunction (d){
     let year = d.getFullYear(); 
     return `${day}, ${date} ${month} ${year}`;
 }
+let x = 10;
